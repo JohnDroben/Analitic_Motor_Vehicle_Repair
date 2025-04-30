8af82bfa07e15934795d5dae0c0c10726bc607ea
DrGorge <drobeng68@gmail.com>
2025-04-30 18:03:22 +0300

    add new code

---
+++ task_1.py
@@ -0,0 +1,11 @@
+# Скачайте любой датасет с сайта https://www.kaggle.com/datasets
+# Загрузите набор данных из CSV-файла в DataFrame.
+# Выведите первые 5 строк данных, чтобы получить представление о структуре данных.
+# Выведите информацию о данных (.info()) и статистическое описание (.describe()).
+
+import pandas as pd
+
+df = pd.read_csv('Motor_Vehicle_Repair_and_Towing.csv', sep=',')
+print(df.head())
+print(df.info())
+print(df.describe())
